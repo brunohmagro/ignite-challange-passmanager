36d8b152acb82474f3d719cf75e219d6c179fdb6
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-06-18 09:04:46 -0300

    feat: challange finished

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from './src/routes/app.routes';
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
-    Poppins_500Medium
+    Poppins_500Medium,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" />
       <AppRoutes />
     </NavigationContainer>
   );
